39c4cebddbc9ffa4925d7c5f9fa350308fb85adf
Askarda <askarda@yandex.ru>
2020-07-01 14:54:19 +0300

    first commit

---
+++ js/screept.js
@@ -0,0 +1,90 @@
+"use strict";
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number =10;
+// console.log(number);
+// //  console/log(number_2);
+// //  var number_2 =20;
+// let message;
+// message = 'hello';
+// console.log(message);
+// // alert(message);
+// // let w;
+// // console.log(w);
+// // typeof 0;
+// console.log(typeof(0));
+// // console.log(3/0);
+// // console.log('два'*32);
+// let obj ={
+//     user:"Aslan",
+//     password:123,
+//     age:12
+// };
+// console.log(obj.user);
+// // console.log(obj['user']);
+// // alert("Hello WORLD");
+// // const result = confirm("Are you here");
+// // if(result==""){
+// //     alert("hello");
+// // } else{
+// //     alert("Goodbay");
+// // }
+//  console.log(result);
+// // if(result==true){
+//     alert("Hello BRO");
+// }else{
+//     alert("goodbay");
+// }
+// let one =1, two =12;
+// console.log(one,two);
+// let answers = [];
+// answers [0] = prompt('Как вас зовут?', '');
+// answers [1] = prompt('как ваша фамилия?', '');
+// answers [2] = +prompt('Сколько Вам лет', '');
+// document.write(answers);
+// console.log(answers);
+// function d(a,c){
+// /* */
+// };
+// console.log(a.length);
+function d(v,r) {
+    
+};
+let a = [1,2,'Hello'];
+console.log(typeof(a));
+console.log(typeof(d));
+console.log(d.length);
+// Интерполяция
+const category = 'toys';
+console.log(`https://askarda.com/${category}/123`);
+
+let base = 1024;
+let table = [base, base+1,base+2];
+// console.log(table[0],table[1],table[2]);
+console.log(table);
+let a = [1, 2, 3];
+let b = [0, ...a, 4];
+console.log(b);
+let digits = [..."0123456789ABCDEF"];
+console.log(digits);
+let digits1 =  ["0123456789ABCDEF"];
+console.log(digits1);
+let arr =[];
+console.log(typeof(arr));
+arr[0]=1;
+arr[2]=3;
+console.log(arr[1]);
+// let ad = "foo";
+
+// var  сd  =  аd; 
+// // разбиение  ·а·  на  массив символов 
+// cd.split(  ""  ); 
+// // переставить массив символов в  обратном порядке 
+// cd.reverse();
+// // снова объединить массив символов в  строку 
+// cd.join(  ""  ); 
+//  //  "oof
+//  console.log(cd);
+
+
